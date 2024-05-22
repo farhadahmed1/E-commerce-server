cbb6e5ec4c236ba9e31b7a42e8703cf811c4f85a
farhadahmed1 <farhad.ahmed.aust@gmail.com>
2024-05-22 13:56:39 +0600

    added README file

--- dist/app.js
+++ dist/app.js
@@ -5,12 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const product_route_1 = require("./app/modules/product/product.route");
+const order_route_1 = require("./app/modules/orders/order.route");
 const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.use(express_1.default.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// application routes
+app.use('/api/products', product_route_1.ProductRoutes);
+app.use('/api/orders', order_route_1.OrderRoutes);
+app.get('/', (req, res) => {
+    res.send('Hello e-commerce World!');
 });
 exports.default = app;
